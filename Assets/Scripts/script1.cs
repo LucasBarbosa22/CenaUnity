4b1a457d0c745f0bf6d459793f90bf65c7062a0d
Lucas Barreto Barbosa <lucas.barbosa275@etec.sp.gov.br>
2022-05-22 15:11:37 -0300

    Alteracoes2205

---
+++ Assets/Scripts/script1.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script1 : MonoBehaviour
+{
+
+    public Vector3 ball;
+
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            ball.x = 0f;
+            ball.y = 0f;
+            ball.z = 0.04f;
+
+            transform.Translate(ball);
+        }
+
+        if(Input.GetKey(KeyCode.S))
+        {
+            ball.x = 0f;
+            ball.y = 0f;
+            ball.z = -0.03f;
+
+            transform.Translate(ball);
+        }
+
+        if(Input.GetKey(KeyCode.A))
+        {
+            ball.x = 0f;
+            ball.y = -0.3f;
+            ball.z = 0f;
+
+            transform.Rotate(ball);
+        }
+
+        if(Input.GetKey(KeyCode.D))
+        {
+            ball.x = 0f;
+            ball.y = 0.3f;
+            ball.z = 0f;
+
+            transform.Rotate(ball);
+        }
+    }
+}
